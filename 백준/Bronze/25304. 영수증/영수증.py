a1c57fb1cf87fd7a3030c7ac44d3b52d2f738c34
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-12 17:38:09 +0100

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+x = int(input())
+n = int(input())
+
+sum = 0
+
+for i in range(n):
+    a, b = map(int, input().split())
+    sum += a * b
+    
+if x == sum:
+    print('Yes')
+else:
+    print('No')
