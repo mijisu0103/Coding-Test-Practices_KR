325e2ebcfcdf29f24a287124ef7ea5861d8adeed
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-13 18:17:53 +0100

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+n = int(input())
+nlist = list(map(int, input().split()))
+count = 0
+
+for x in nlist:
+    for i in range(2, x+1):
+        if x % i == 0:
+            if x == i:
+                count += 1
+            break
+            
+print(count)
