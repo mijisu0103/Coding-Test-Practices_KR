3665cf78020839b34779208382bb6fea004b24e4
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-13 17:55:21 +0100

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+arr = []
+
+for i in range(1, n+1):
+    if(n%i == 0):
+        arr.append(i)
+        
+if (k<=len(arr)):
+    print(arr[k-1])
+    
+else: 
+    print(0)
