272eb68bd33a35c788566af53ae41e0dbc5dca90
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-13 19:05:51 +0100

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,16 @@
+x_points = []
+y_points = []
+
+for _ in range(3):
+    x, y = map(int, input().split())
+    x_points.append(x)
+    y_points.append(y)
+
+for i in range(3):
+    if x_points.count(x_points[i]) == 1:
+        x4 = x_points[i]
+    if y_points.count(y_points[i]) == 1:
+        y4 = y_points[i]
+
+print(x4, y4)
+        
