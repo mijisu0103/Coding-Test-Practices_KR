6a6e6255ab15c14fbec0a90611bf2f41477257a3
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-16 01:29:48 +0100

    [Silver III] Title: 도키도키 간식드리미, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+wait = list(map(int, input().split()))
+
+tmp = []
+
+target = 1
+for i in wait:
+    tmp.append(i)
+    while tmp and tmp[-1] == target:
+        tmp.pop()
+        target += 1
+    if len(tmp) > 1 and tmp[-1] > tmp[-2]:
+        print("Sad")
+        sys.exit()
+if tmp:
+    print("Sad")
+else:
+    print("Nice")
