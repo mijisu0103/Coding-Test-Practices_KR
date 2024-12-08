fdd7dc7c2a8efb47b451092e9b8fa4781a426d2e
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-12-08 23:28:31 +0000

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,20 @@
+x = int(input())
+
+case = list(map(int, input().split()))
+reverse_case = case[::-1]
+
+increase = [1 for i in range(x)] 
+decrease = [1 for i in range(x)] 
+
+for i in range(x):
+    for j in range(i):
+        if case[i] > case[j]:
+            increase[i] = max(increase[i], increase[j] + 1)
+        if reverse_case[i] > reverse_case[j]:
+            decrease[i] = max(decrease[i], decrease[j] + 1)
+
+result = [0 for i in range(x)]
+for i in range(x):
+    result[i] = increase[i] + decrease[x - i - 1] - 1
+
+print(max(result))
