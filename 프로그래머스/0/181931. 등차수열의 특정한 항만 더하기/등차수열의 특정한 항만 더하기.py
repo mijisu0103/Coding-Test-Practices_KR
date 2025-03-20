7289b636f38822476331fc4dc58a066b563f69da
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-03-20 10:03:14 +0000

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,8 @@
+def solution(a, d, included):
+    answer = 0
+    
+    for i in range(len(included)):
+        if included[i] == True:
+            answer += a + i * d
+            
+    return answer
