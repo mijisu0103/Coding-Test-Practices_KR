7c2cf1bde6e5470ba76a9740cbe3c2a943bdb4cd
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-03-21 15:01:45 +0000

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.20 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,8 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        if (i ** 0.5).is_integer():
+            answer -= i
+        else:
+            answer += i
+    return answer
