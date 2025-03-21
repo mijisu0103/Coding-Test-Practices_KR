8fbf7480795a25bd4a8ab4e8489e5162a1c69cad
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-03-21 16:54:52 +0000

    [level 2] Title: 이진 변환 반복하기, Time: 2.08 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = []
+    cnt = 0     
+    zeroCnt = 0
+    
+    while True:
+        if s == "1":
+            break;
+            
+        zeroCnt += s.count("0")  #문자열의 0의 개수 세기
+        s = s.replace("0",'')    #0을 공백으로 변환
+        s = bin(len(s))[2:]      #2진수로 변환
+        
+        cnt +=1
+        
+    answer = [cnt, zeroCnt]    
+    return answer
