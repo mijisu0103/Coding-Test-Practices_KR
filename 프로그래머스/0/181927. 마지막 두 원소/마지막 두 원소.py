cad6c03698b6f9b1e6655ff5628e21cf9bbed389
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-03-24 03:52:51 +0000

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+
+    if num_list[-1] > num_list[-2]:
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(2 * num_list[-1])
+    
+    return num_list
