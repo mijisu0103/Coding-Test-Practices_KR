2e1cdfb8fe202b91a8ab94e57acf7deed2060db4
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-03-25 17:07:18 +0000

    [level 0] Title: 수열과 구간 쿼리 4, Time: 12.40 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,9 @@
+def solution(arr, queries):
+    answer = [] 
+    
+    for s, e, k in queries: 
+        for i in range(s, e+1):
+            if i % k == 0:
+                arr[i] += 1
+    
+    return arr
