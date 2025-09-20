d291b141087bb208faab2300d65971d99014fe54
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-09-20 09:29:22 +0100

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,9 +1,14 @@
-sn = [0]*31
+check = [0] * 31
 
 for i in range(28):
-    v = int(input())
-    sn[v] = 1
+    n = int(input())
+    check[n] = 1
     
+answer = []
+
 for i in range(1, 31):
-    if sn[i] == 0:
-        print(i)
+    if check[i] == 0:
+        answer.append(i)
+        
+print(answer[0])
+print(answer[1])
