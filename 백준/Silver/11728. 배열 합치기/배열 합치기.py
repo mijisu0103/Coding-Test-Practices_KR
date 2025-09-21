e9d3e0c9d88fe6080480585caf85da84855b805e
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-09-21 16:24:13 +0100

    [Silver V] Title: 배열 합치기, Time: 2216 ms, Memory: 188028 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = []
+
+pos1 = 0
+pos2 = 0
+
+while pos1 < N and pos2 < M:
+    candidate1 = A[pos1]
+    candidate2 = B[pos2]
+    
+    if candidate1 < candidate2:
+        C.append(candidate1)
+        pos1 += 1
+    else:
+        C.append(candidate2)
+        pos2 += 1
+
+if pos1 != N:
+    C.extend(A[pos1:N])
+if pos2 != M:
+    C.extend(B[pos2:M])
+
+for i in range(N + M):
+    print(C[i], end = " ")
