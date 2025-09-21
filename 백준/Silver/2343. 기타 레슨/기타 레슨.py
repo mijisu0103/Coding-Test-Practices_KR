839d8efc7276de42fdf2a8565ba582c5bfb194c5
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-09-21 15:31:17 +0100

    [Silver I] Title: 기타 레슨, Time: 688 ms, Memory: 42168 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+running_time = list(map(int, input().split()))
+
+left = max(running_time)
+right = sum(running_time)
+answer = -1
+
+while left <= right:
+    middle = (left + right) // 2
+    
+    blueray_num = 1
+    remain = middle
+    
+    for i in range(N):
+        if remain < running_time[i]:
+            blueray_num += 1
+            remain = middle
+            
+        remain -= running_time[i]
+    
+    if blueray_num <= M:
+        answer = middle
+        right = middle -1
+        
+    else:
+        left = middle + 1
+        
+print(answer)        
