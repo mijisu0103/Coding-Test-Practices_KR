ebcd42f652468fe4f2512872e7475ffdc31c3fee
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-09-22 17:18:52 +0100

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 152 ms, Memory: 35668 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for _ in range(T):
+    N, M = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    
+    A = sorted(A)
+    B = sorted(B)
+    
+    main = 0
+    sub = 0
+    count = 0
+    
+    while main < N:
+        if sub == M:
+            count += sub
+            main += 1
+        else:
+            if A[main] > B[sub]:
+                sub += 1
+            else:
+                count += sub
+                main += 1
+                
+    print(count)            
