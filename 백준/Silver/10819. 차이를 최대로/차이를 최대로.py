f571c74b853a7012cfd37e6cfceef14f533c97af
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-09-22 11:54:00 +0100

    [Silver II] Title: 차이를 최대로, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+N = int(input())
+A = list(map(int, input().split()))
+
+max_diff_sum = 0
+
+for a in permutations(A, N):
+    diff_sum = 0
+    for i in range(N - 1):
+        diff_sum += abs(a[i] - a[i+1])
+    
+    max_diff_sum = max(max_diff_sum, diff_sum)
+    
+print(max_diff_sum)
