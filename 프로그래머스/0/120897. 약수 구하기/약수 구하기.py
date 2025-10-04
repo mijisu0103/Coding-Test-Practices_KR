40695ca7f267bbe0e53bedd29da8e4f1d0519a31
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-10-04 15:08:16 +0100

    [level 0] Title: 약수 구하기, Time: 0.07 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(n):
+    divisors = []
+    
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    
+    return sorted(divisors)
