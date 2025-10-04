09c40f6dead0d0b136441afbaa95c1c8a4670ce6
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-10-04 10:12:37 +0100

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = [0] * 2
+    
+    for i in num_list:
+        if int(i) % 2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1
+            
+    return answer
