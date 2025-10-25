1664073e21e640d97e013f965c2a00c92a760e0c
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2025-10-25 23:49:16 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.55 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -0,0 +1,39 @@
+def solution(numbers, hand):
+    answer = ''
+    key_dict = {1:(0,0), 2:(0,1), 3:(0,2),
+                4:(1,0), 5:(1,1), 6:(1,2),
+                7:(2,0), 8:(2,1), 9:(2,2),
+                '*':(3,0), 0:(3,1), '#':(3,2)}
+
+    left, right = [1,4,7], [3,6,9]
+    lhand, rhand = '*', '#'
+    
+    for i in numbers:
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
+        else:
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0] - lPos[0]) + abs(curPos[1] - lPos[1])
+            rdist = abs(curPos[0] - rPos[0]) + abs(curPos[1] - rPos[1])
+
+            if ldist < rdist:
+                answer += 'L'
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    lhand = i
+                else:
+                    answer += 'R'
+                    rhand = i
+
+    return answer
