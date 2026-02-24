ea36325ce7570e96cc7fad978b049255d6ddaec9
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2026-02-25 00:33:08 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers, n):
+    answer = 0
+    
+    for i in numbers:
+        if answer > n:
+            return answer
+        else:
+            answer += i
+        
+    
+    return answer
