7c853f1bdc505b9634174a6ea2c267d0f2b1f362
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2026-04-28 23:11:18 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    
+    while chicken >= 10:
+        
+        div, mod = divmod(chicken, 10)
+        answer += div
+        chicken = div + mod
+    
+    return answer
